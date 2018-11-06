2c130b2185ebf166204c77b1359f056db3d7d79d
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-11-06 14:56:28 -0500

    yeet

--- src/WordJumble.java
+++ src/WordJumble.java
@@ -1,37 +1,31 @@
-
+/*
+* Made by corbin nash
+*/
 import javax.swing.JOptionPane;
-
-
 public class WordJumble {
   public static void main(String[] args) {
-    String letters = JOptionPane.showInputDialog("Enter a word");
-    int num;      
+   //allow use to input a word
+   String letters = JOptionPane.showInputDialog("Enter a word");    
     jumbleWords(letters, "");
   }
-
-  //input parameters
+  //input parameters:
   //word - the remaining letters in the word still to jumble
-  //jumbLet - the letters already used to create
-  //the jumbled word
+  //jumbLet - the letters already used to create the jumbled word
   public static void jumbleWords(String word, String jumbLet){
-    int pos,num;
+    int pos;
     String remainingLetters;
     String origWord = word;
     String origJumbledLetters = jumbLet;
     if (word.length() == 1) {
-     System.out.println(jumbLet + word);
-    } else {
+        System.out.println(jumbLet + word); 
+    } 
+    else {
       for (pos = 0; pos < origWord.length(); pos++) {
           remainingLetters = origWord.substring(0, pos)
           + origWord.substring(pos + 1, origWord.length());
         //recursive call to jumbleWords()
-          jumbleWords(remainingLetters, 
-          origJumbledLetters + origWord.charAt(pos));
-      
+          jumbleWords(remainingLetters,origJumbledLetters + origWord.charAt(pos));
       }
-    
     }
-   
   }
-
 }
